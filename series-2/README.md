7952a05f68a979050661df7733fff8cf61ad94f6
Omer-Demirtas <gamega55834@gmail.com>
2022-10-12 20:47:42 +0300

    Some code updates.

--- series-2/README.md
+++ series-2/README.md
@@ -2,6 +2,12 @@
 # Contents
 
 ### Scaling
+to scale image;
+```
+scale = cv2.resize(img, (int(width / 2), int(height / 2)), interpolation=cv2.INTER_CUBIC)
+```
+
+
 ### Rotating
 ### Shifting 
 ### Edge Detection
--- series-2/main.py
+++ series-2/main.py
@@ -8,7 +8,7 @@ img = cv2.imread("../images/image.jpg")
 # Specify the size of image along with interploation methods.
 # cv2.INTER_AREA is used for shrinking, whereas cv2.INTER_CUBIC
 # is used for zooming.
-res = cv2.resize(img, (int(width / 2), int(height / 2)), interpolation=cv2.INTER_CUBIC)
+scale = cv2.resize(img, (int(width / 2), int(height / 2)), interpolation=cv2.INTER_CUBIC)
 
 #cv2.imshow("resize", res)
 
@@ -18,7 +18,7 @@ res = cv2.warpAffine(img, M, (width, height))
 
 cv2.imshow("image", img)
 cv2.imshow("res", res)
-
+cv2.imshow("Scale", scale)
 
 if cv2.waitKey(0) & 0xff == 27:
     cv2.destroyAllWindows()
